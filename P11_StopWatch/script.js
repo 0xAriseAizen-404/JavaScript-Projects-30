b476a43a0029a1d760994f7a8f377a9ed9fc7db2
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-04-17 23:12:30 +0530

    project -1

---
+++ P11_StopWatch/script.js
@@ -0,0 +1,46 @@
+const display = document.querySelector(".display");
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+
+let startTime,
+  elapsedTime = 0,
+  intervalId;
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  intervalId = setInterval(updateTime, 10);
+}
+
+function stop() {
+  clearInterval(intervalId);
+  elapsedTime = Date.now() - startTime;
+}
+
+function reset() {
+  clearInterval(intervalId);
+  elapsedTime = 0;
+  display.textContent = "00:00:00";
+}
+
+function updateTime() {
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  updateDisplay(elapsedTime);
+}
+
+function updateDisplay(time) {
+  const seconds = Math.floor(time / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const formattedTime = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+  display.textContent = formattedTime;
+}
+
+function pad(num) {
+  return num < 10 ? "0" + num : num;
+}
